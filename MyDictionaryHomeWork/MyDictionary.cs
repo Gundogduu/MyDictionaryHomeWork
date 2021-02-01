88819483c057f85083eb7c4846fadd219818496e
Hayri Can GÜNDOĞDU <hc_gundogdu@aof.anadolu.edu.tr>
2021-02-01 23:29:35 +0300

    Add project files.

---
+++ MyDictionaryHomeWork/MyDictionary.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyDictionaryHomeWork
+{
+    class MyDictionary<T>
+    {
+        T[] _array;       //class  cagırıldıgında direkt olusan bos liste
+        T[] _tempArray;   //gecici liste
+        public MyDictionary()
+        {
+            _array = new T[0];       //constructor icinde newledik cünkü direkt calismasini istedik.
+        }
+        public void Add(T item)    //item kullanıcının gönderdigi string
+        { //burada _array'i geciciliste'ye aktardık.Cünkü her eleman eklendiginde bellekte yeni bir referans no alacak ve eski no'daki elemanlar kaybolacak.Bunu engellemek icin.
+            _tempArray = _array;
+            //burada yeni bir _array listesi olusturduk ve eleman sayını 1 artır dedik.
+            _array = new T[_array.Length + 1];
+            //burada geciciliste emanet ettigimiz elemanları _array listesine geri aldık.
+            for (int i = 0; i < _tempArray.Length; i++)
+            {
+                _array[i] = _tempArray[i];
+            }
+            //kullanıcının gonderdigini dizinin son elemanına ekliyoruz.
+            _array[_array.Length - 1] = item;
+        }
+
+        //kendi Count attribute'umuzu yaptık.
+        //_array listesinin eleman sayısını getir dedik.
+        public int Count
+        {
+            get { return _array.Length; }
+        }
+
+        public void GetAll()
+        {
+            Console.WriteLine("----İsimler----");
+            foreach (T isim in _array)
+            {
+                Console.WriteLine(isim);
+            }
+        }
+
+    }
+}
---
+++ MyDictionaryHomeWork/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace MyDictionaryHomeWork
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            MyDictionary<string> isimler = new MyDictionary<string>();
+            isimler.Add("Mahmud Nergis");
+            isimler.Add("Hayri Gündoğdu");
+            isimler.Add("Ebru Demir");
+            isimler.Add("Malhun Çiçek");
+            Console.WriteLine("Eleman sayısı: " + isimler.Count);
+            isimler.GetAll();
+
+        }
+    }
+}
